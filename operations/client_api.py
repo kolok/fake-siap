07f2512a98e3f85561fa0bb3f5e12d428544fa91
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-01-05 12:24:56 +0100

    create apilos programme after operation creation

--- operations/client_api.py
+++ operations/client_api.py
@@ -12,5 +12,40 @@ class ClientAPI():
         self.client = coreapi.Client(auth=auth)
 
     def get_conventions(self):
-        print(settings.APILOS_API_CLIENT_HOST + 'conventions')
+        print(settings.APILOS_API_CLIENT_HOST + 'conventions/')
         return self.client.get(settings.APILOS_API_CLIENT_HOST + 'conventions')
+
+    def create_programme(self, payload):
+        print(f"CREATE PROGRAMME : {settings.APILOS_API_CLIENT_HOST + 'programmes/'}")
+        programme = requests.post(
+            settings.APILOS_API_CLIENT_HOST + 'programmes/',
+            auth=(
+                settings.APILOS_API_CLIENT_USERNAME,
+                settings.APILOS_API_CLIENT_PASSWORD,
+            ),
+            allow_redirects=False,
+            data=payload
+        )
+        print(programme)
+        if programme.status_code == 201:
+            return programme.json()
+
+    def create_lot(self, payload):
+        print(f"CREATE LOT : {settings.APILOS_API_CLIENT_HOST + 'lots/'}")
+        lot = requests.post(
+            settings.APILOS_API_CLIENT_HOST + 'lots/',
+            auth=(
+                settings.APILOS_API_CLIENT_USERNAME,
+                settings.APILOS_API_CLIENT_PASSWORD,
+            ),
+            allow_redirects=False,
+            data=payload
+        )
+        if lot.status_code == 201:
+            return lot.json()
+
+
+
+
+
+
--- operations/views.py
+++ operations/views.py
@@ -13,7 +13,6 @@ client_api = ClientAPI()
 @login_required
 def operation_list(request):
     operations = Operation.objects.all()
-    print('OP')
     return render(
         request,
         "operations/operation_list.html",
@@ -23,7 +22,6 @@ def operation_list(request):
 @login_required
 def convention_list(request):
     convention_list = client_api.get_conventions()
-    print('CONV')
     return render(
         request,
         "operations/convention_list.html",
@@ -36,7 +34,51 @@ class OperationCreate(CreateView):
   form_class = OperationForm
   def form_valid(self, form):    
     super(OperationCreate, self).form_valid(form)
-    print('CREATE : Après form validate !!!')
+    payload = {
+      'nom' : form.cleaned_data["nom"],
+      'bailleur_uuid' : f'{form.cleaned_data["bailleur"].apilos_uuid}',
+      'administration_uuid' : f'{form.cleaned_data["administration"].apilos_uuid}',
+      'code_postal' : form.cleaned_data["code_postal"],
+      'ville' : form.cleaned_data["ville"],
+      'adresse' : form.cleaned_data["adresse"],
+      'numero_galion' : form.cleaned_data["numero_galion"],
+      'annee_gestion_programmation' : form.cleaned_data["annee_gestion_programmation"],
+      'zone_123' : form.cleaned_data["zone_123"],
+      'zone_abc' : form.cleaned_data["zone_abc"],
+      'surface_utile_totale' : f'{form.cleaned_data["surface_utile_totale"]}',
+      'type_habitat' : form.cleaned_data["type_habitat"],
+      'type_operation' : form.cleaned_data["type_operation"],
+      'date_achevement_previsible' : f'{form.cleaned_data["date_achevement_previsible"]}',
+#    'plai', 'plus', 'pls'
+    }
+    print(form.cleaned_data)
+    print(self.object)
+    programme = client_api.create_programme(payload)
+    self.object.apilos_uuid = programme['uuid']
+    self.object.save()
+
+    if form.cleaned_data["plai"]:
+      client_api.create_lot({
+        'programme_uuid': programme['uuid'],
+        'financement': 'PLAI',
+        'nb_logements': 10
+      })
+    if form.cleaned_data["plus"]:
+      client_api.create_lot({
+        'programme_uuid': programme['uuid'],
+        'financement': 'PLUS',
+        'nb_logements': 10
+      })
+    if form.cleaned_data["pls"]:
+      client_api.create_lot({
+        'programme_uuid': programme['uuid'],
+        'financement': 'PLS',
+        'nb_logements': 10
+      })
+
+    print('CREATE PROGRAMME : Après form validate !!!')
+    print(payload)
+    print(self)
     return redirect('operations:operation_list')
 
 class OperationUpdate(UpdateView):
